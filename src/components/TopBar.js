a97bd38c865d490f8974aa419b13d709db8ff609
ojas-cashify <74178526+ojas-cashify@users.noreply.github.com>
2021-08-27 19:41:04 +0530

    Update .gitignore file and removed unused apis from TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,7 +7,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import {useHistory} from "react-router";
-import {LogoutUser} from "../apiCalls/api-calls";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,7 +26,6 @@ export default function TopBar({name}) {
 
     const onLogOut =() =>{
 
-        const token = localStorage.getItem('cal-token')
         alert("User Logged Out")
         localStorage.clear()
         history.push("/")
